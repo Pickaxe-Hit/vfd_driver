8e5d8b9f52e3c80eeef55245a6e0922d3e1fba0b
Pickaxe-Hit <pickaxe_hit@qq.com>
2022-05-08 19:05:34 +0800

    Update test function

--- main/main.c
+++ main/main.c
@@ -1,5 +1,17 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "mn12832j.h"
+
+void draw(void) {
+    for (;;) {
+        vfd_draw_square(0, 0, 127, 127, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vfd_draw_square(0, 0, 127, 127, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
 
 void app_main(void) {
     vfd_start_send();
+    xTaskCreatePinnedToCore(draw, "draw", 2048, NULL, 2, NULL, 0);
 }
